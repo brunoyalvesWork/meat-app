d6e141e81cbd7ad3b965796a497170db490f5f38
Bruno Yonezawa Sato <bruno.sato@venturus.org.br>
2017-11-07 13:28:17 -0200

    Alteração no package.json e criação de novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-
+import { ROUTES } from './app-routes';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { RestaurantComponent } from './restaurantes/restaurant/restaurant.component'
 
 
 @NgModule({
@@ -15,12 +18,15 @@ import { HomeComponent } from './home/home.component'
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    RestaurantesComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
