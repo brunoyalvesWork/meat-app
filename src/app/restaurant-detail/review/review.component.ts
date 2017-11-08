4961d6d7bf6f55aca6c66324d940912ea8fd3de8
Bruno Yonezawa Sato <bruno.sato@venturus.org.br>
2017-11-08 08:56:17 -0200

    Itens do menu dinamicos

--- src/app/restaurant-detail/review/review.component.ts
+++ src/app/restaurant-detail/review/review.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantService } from './../../restaurantes/restaurants.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'mt-review',
@@ -6,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>;
+
+
+  constructor(
+    private restaurantsService: RestaurantService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(this.route.parent.snapshot.params['id']);
   }
 
 }
--- src/app/restaurantes/restaurants.service.ts
+++ src/app/restaurantes/restaurants.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
+import { MenuItem } from './../restaurant-detail/menu-item/menu-item.model';
 import { Restaurant } from './restaurant/restaurant.model';
 import { MEAT_API } from './../app.api';
 
@@ -26,5 +27,17 @@ export class RestaurantService {
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError);
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurants(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
     }
 }
