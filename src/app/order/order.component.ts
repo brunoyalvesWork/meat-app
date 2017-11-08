75134555e36c25d4ce574d041958d2878ddcc02b
Bruno Yonezawa Sato <bruno.sato@venturus.org.br>
2017-11-08 14:13:19 -0200

    Validators no reactiveForms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 import { CartItem } from './../restaurant-detail/shopping-car/cart-item.model';
 import { OrderService } from './order.service';
@@ -29,19 +29,32 @@ export class OrderComponent implements OnInit {
   constructor(
     private orderService: OrderService,
     private router: Router,
-    private formBuilder: FormBuilder
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
-    this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control('', [ Validators.required, Validators.minLength(5) ]),
-      email: this.formBuilder.control('', [ Validators.required, Validators.pattern(this.emailPattern) ]),
-      emailConfirmation: this.formBuilder.control('', [ Validators.required, Validators.pattern(this.emailPattern) ]),
-      address: this.formBuilder.control('', [ Validators.required, Validators.minLength(5) ]),
-      number: this.formBuilder.control('', [ Validators.required, Validators.pattern(this.numberPatern) ]),
-      optionalAddress: this.formBuilder.control(''),
-      paymentOption: this.formBuilder.control('', [ Validators.required ])
-    })
+    this.orderForm = this.fb.group({
+      name: this.fb.control('', [ Validators.required, Validators.minLength(5) ]),
+      email: this.fb.control('', [ Validators.required, Validators.pattern(this.emailPattern) ]),
+      emailConfirmation: this.fb.control('', [ Validators.required, Validators.pattern(this.emailPattern) ]),
+      address: this.fb.control('', [ Validators.required, Validators.minLength(5) ]),
+      number: this.fb.control('', [ Validators.required, Validators.pattern(this.numberPatern) ]),
+      optionalAddress: this.fb.control(''),
+      paymentOption: this.fb.control('', [ Validators.required ])
+    }, { validator:  OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean}{
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation'); 
+
+    if(!email || !emailConfirmation){
+      return undefined;
+    }
+    if (email.value !== emailConfirmation.value) {
+      return{ emailsNotMatch: true };
+    } 
+    return undefined;
   }
 
   itemsValue(): number {
